04c38b4876f32c208442ee75f9d24581f99018ea
scar1377 <scarlettadams77@gmail.com>
2021-12-06 12:15:09 +0000

    add content to HomeScreen

--- Routes/MainStack.js
+++ Routes/MainStack.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../Screens/HomeScreen";
+import ListSpacesScreen from "../Screens/ListSpacesScreen";
 import React from "react";
 
 const Stack = createStackNavigator();
@@ -8,6 +9,7 @@ export default function MainStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Spaces" component={ListSpacesScreen} />
     </Stack.Navigator>
   );
 }
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,36 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  const [location, setLocation] = useState("");
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View>
+        <Text>HomeScreen</Text>
+        <TextInput
+          style={styles.inputContainer}
+          placeholder="Postcode"
+          value={location}
+          onChangeText={(text) => setLocation(text)}
+        />
+        <Button
+          title="Browse"
+          onPress={() => {
+            navigation.navigate("Spaces");
+          }}
+        />
+      </View>
+      <View>
+        <Button title="Make a space" />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    borderWidth: 1,
+  },
+});
 
 export default HomeScreen;
