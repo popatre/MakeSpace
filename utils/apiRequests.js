81ee5d0bc265f2bb49b20003c2ff11c84f92dab1
scar1377 <scarlettadams77@gmail.com>
2021-12-09 11:38:48 +0000

    add utils functions of posting new listing and getting location for each listing

--- utils/apiRequests.js
+++ utils/apiRequests.js
@@ -1,11 +1,29 @@
 import axios from "axios";
 
 const makespaceApi = axios.create({
-    baseURL: "https://make-space.herokuapp.com/api",
+  baseURL: "https://make-space.herokuapp.com/api",
+});
+
+const postcodesApi = axios.create({
+  baseURL: "https://api.postcodes.io/postcodes/",
 });
 
 export const getAllListings = () => {
-    return makespaceApi.get("/listings").then((res) => {
-        return res.data.listings;
-    });
+  return makespaceApi.get("/listings").then((res) => {
+    return res.data.listings;
+  });
+};
+
+export const postListing = (newListing) => {
+  return makespaceApi.post("/listings", newListing).then((res) => {
+    console.log(res.data);
+    return res.data;
+  });
+};
+
+export const getLocation = (postcode) => {
+  return postcodesApi.get(`/${postcode}`).then((res) => {
+    // console.log(res.data.result, "<<<<<<<<<<returning location data");
+    return res.data.result;
+  });
 };
