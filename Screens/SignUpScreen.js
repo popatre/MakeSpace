81217c916594081826b5111843187ca38a64fdca
popatre <76402245+popatre@users.noreply.github.com>
2021-12-10 10:45:31 +0000

    add signup with connection to mongodb

--- Screens/SignUpScreen.js
+++ Screens/SignUpScreen.js
@@ -1,136 +1,131 @@
 import React, { useState, useEffect } from "react";
-import { withFormik } from "formik";
-// import {
-//     getAuth,
-//     createUserWithEmailAndPassword,
-//     signInWithEmailAndPassword,
-//     onAuthStateChanged,
-// } from "firebase/auth";
-import {
-  KeyboardAvoidingView,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-// import { auth } from "../firebase";
-const SignUpScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [ConfirmPassword, setConfirmPassword] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [username, setUsername] = useState("");
-  //     const navigation = useNavigation();
-  //     useEffect(() => {
-  //         const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //             if (user) {
-  //                 navigation.navigate("home");
-  //             }
-  //         });
-  //         return unsubscribe;
-  //     }, []);
 
-  // const handleSignUp = () => {
-  //     createUserWithEmailAndPassword(auth, email, password)
-  //         .then((userCred) => {
-  //             const user = userCred.user;
-  //             console.log("registered with ", user);
-  //         })
-  //         .catch((error) => alert(error.message));
-  // };
-  // const handleLogin = () => {
-  //     signInWithEmailAndPassword(auth, email, password)
-  //         .then((userCred) => {
-  //             const user = userCred.user;
-  //             console.log(user);
-  //         })
-  //         .catch((error) => alert(error.message));
-  // };
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Username"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-          style={styles.input}
-        ></TextInput>
-        <TextInput
-          placeholder="Display Name"
-          value={displayName}
-          onChangeText={(text) => setDisplayName(text)}
-          style={styles.input}
-        ></TextInput>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          style={styles.input}
-        ></TextInput>
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        ></TextInput>
-        <TextInput
-          placeholder="Confirm Password"
-          value={ConfirmPassword}
-          onChangeText={(text) => setConfirmPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        ></TextInput>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            navigation.replace("Login");
-          }}
-        >
-          <Text style={styles.buttonText}>Sign Up</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  );
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+} from "firebase/auth";
+import {
+    KeyboardAvoidingView,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+
+import { postUser } from "../utils/apiRequests";
+
+import { auth } from "../firebase";
+const SignUpScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [username, setUsername] = useState("");
+    //     const navigation = useNavigation();
+    //     useEffect(() => {
+    //         const unsubscribe = onAuthStateChanged(auth, (user) => {
+    //             if (user) {
+    //                 navigation.navigate("home");
+    //             }
+    //         });
+    //         return unsubscribe;
+    //     }, []);
+
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCred) => {
+                const uid = userCred.user.uid;
+                postUser({
+                    _id: uid,
+                    username: username,
+                    displayName: displayName,
+                    emailAddress: email,
+                }).then(() => {
+                    navigation.replace("Home");
+                });
+            })
+            .catch((error) => alert(error.message));
+    };
+
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={(text) => setUsername(text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
+                    placeholder="Display Name"
+                    value={displayName}
+                    onChangeText={(text) => setDisplayName(text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    style={styles.input}
+                ></TextInput>
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                ></TextInput>
+                <TextInput
+                    placeholder="Confirm Password"
+                    value={ConfirmPassword}
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                ></TextInput>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 };
 export default SignUpScreen;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputContainer: { width: "80%" },
-  input: {
-    backgroundColor: "white",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  buttonContainer: {
-    width: "60%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "100%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  buttonOutline: {
-    backgroundColor: "white",
-    marginTop: 5,
-    borderColor: "#0782F9",
-    borderWidth: 2,
-  },
-  buttonOutlineText: { color: "#0782F9", fontWeight: "700", fontSize: 16 },
-  buttonText: { color: "white", fontWeight: "700", fontSize: 16 },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputContainer: { width: "80%" },
+    input: {
+        backgroundColor: "white",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: "#0782F9",
+        width: "100%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+    },
+    buttonOutline: {
+        backgroundColor: "white",
+        marginTop: 5,
+        borderColor: "#0782F9",
+        borderWidth: 2,
+    },
+    buttonOutlineText: { color: "#0782F9", fontWeight: "700", fontSize: 16 },
+    buttonText: { color: "white", fontWeight: "700", fontSize: 16 },
 });
--- utils/apiRequests.js
+++ utils/apiRequests.js
@@ -1,32 +1,39 @@
 import axios from "axios";
 
 const makespaceApi = axios.create({
-  baseURL: "https://make-space.herokuapp.com/api",
+    baseURL: "https://make-space.herokuapp.com/api",
 });
 
 const postcodesApi = axios.create({
-  baseURL: "https://api.postcodes.io/postcodes/",
+    baseURL: "https://api.postcodes.io/postcodes/",
 });
 
 export const getAllListings = (sort) => {
-  return makespaceApi
-    .get("/listings", { params: { sortby: sort } })
-    .then((res) => {
-      console.log(res.data.listings, "<<<res.data");
-      return res.data.listings;
-    });
+    return makespaceApi
+        .get("/listings", { params: { sortby: sort } })
+        .then((res) => {
+            console.log(res.data.listings, "<<<res.data");
+            return res.data.listings;
+        });
 };
 
 export const postListing = (newListing) => {
-  return makespaceApi.post("/listings", newListing).then((res) => {
-    console.log(res.data);
-    return res.data;
-  });
+    return makespaceApi.post("/listings", newListing).then((res) => {
+        console.log(res.data);
+        return res.data;
+    });
 };
 
 export const getLocation = (postcode) => {
-  return postcodesApi.get(`/${postcode}`).then((res) => {
-    //console.log(JSON.stringify(res.data), "<<<<<<<<<<returning location data");
-    return JSON.stringify(res.data.result);
-  });
+    return postcodesApi.get(`/${postcode}`).then((res) => {
+        //console.log(JSON.stringify(res.data), "<<<<<<<<<<returning location data");
+        return JSON.stringify(res.data.result);
+    });
+};
+
+export const postUser = (newUser) => {
+    return makespaceApi.post("/users", newUser).then((res) => {
+        console.log(res.data);
+        return res.data;
+    });
 };
