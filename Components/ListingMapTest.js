bbd24ccf4399a2ccc614ad7d9fd79c410fe95731
scar1377 <scarlettadams77@gmail.com>
2021-12-12 20:30:53 +0000

    add currant location to the map view with circle following the related draggable Marker

--- Components/ListingMapTest.js
+++ Components/ListingMapTest.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Image, Alert } from "react-native";
 import MapView, {
   PROVIDER_GOOGLE,
@@ -6,8 +6,40 @@ import MapView, {
   Callout,
   Circle,
 } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function SingleListingMap({ location }) {
+  const [currantLocation, setCurrantLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [pin, setPin] = useState({
+    latitude: 53.472036,
+    longitude: -2.238065,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let thisLocation = await Location.getCurrentPositionAsync({});
+
+      setCurrantLocation({
+        latitude: thisLocation.coords.latitude,
+        longitude: thisLocation.coords.longitude,
+      });
+      setPin({
+        latitude: thisLocation.coords.latitude,
+        longitude: thisLocation.coords.longitude,
+      });
+    })();
+  }, []);
+
   return (
     <View>
       <Text>ListingMap</Text>
@@ -15,21 +47,29 @@ export default function SingleListingMap({ location }) {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={{
-          latitude: 53.473219,
-          longitude: -2.256017,
+          latitude: currantLocation.latitude,
+          longitude: currantLocation.longitude,
           latitudeDelta: 0.09,
           longitudeDelta: 0.0035,
         }}
       >
         <Circle
-          center={{ latitude: 53.473219, longitude: -2.256017 }}
-          radius={1000}
+          center={pin}
+          radius={1600}
           fillColor={"rgba(200,300,200,0.5)"}
         />
+        {/* <Circle center={currantLocation} radius={1600} strokeColor="coral" /> */}
 
         <Marker
-          draggable
-          coordinate={{ latitude: 53.473219, longitude: -2.256017 }}
+          coordinate={currantLocation}
+          draggable={true}
+          onDragStart={(e) => {}}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
         >
           <Callout>
             <Image
