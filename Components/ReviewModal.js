24c5a110a79bc8e0fadd8231656d4ce0a1b80370
popatre <76402245+popatre@users.noreply.github.com>
2021-12-13 21:18:56 +0000

    add styling to review form modal

--- Components/ReviewModal.js
+++ Components/ReviewModal.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import * as yup from "yup";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  TouchableOpacity,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TextInput,
+    TouchableOpacity,
 } from "react-native";
 import { Formik } from "formik";
 import { AntDesign } from "@expo/vector-icons";
@@ -14,86 +14,86 @@ import StarRating from "./StarRating";
 import { patchListingById } from "../utils/apiRequests";
 
 const ReviewSchema = yup.object({
-  // rating: yup.string().required().min(1),
-  // rating: yup.number().positive().required(),
-  // .test("isNumber", "Must be a Number from 1-5", (val) => {
-  //   const num = Number(val);
+    // rating: yup.string().required().min(1),
+    // rating: yup.number().positive().required(),
+    // .test("isNumber", "Must be a Number from 1-5", (val) => {
+    //   const num = Number(val);
 
-  //   if (num > 0 && num <= 5) {
-  //     return num;
-  //   }
-  // }),
+    //   if (num > 0 && num <= 5) {
+    //     return num;
+    //   }
+    // }),
 
-  body: yup.string().required().min(10),
+    body: yup.string().required().min(10),
 });
 
 export default function ReviewModal({ setOpenReviewModal, listing }) {
-  const [value, setValue] = useState("");
-  const [defaultRating, setDefaultRating] = useState(0);
+    const [value, setValue] = useState("");
+    const [defaultRating, setDefaultRating] = useState(0);
 
-  const reviewHandler = ({ rating, body }) => {
-    let totalStars = 0;
-    let totalReviews = listing.reviews.length + 1;
-    listing.reviews.map((review) => {
-      totalStars += review.SpaceRating;
-    });
+    const reviewHandler = ({ rating, body }) => {
+        let totalStars = 0;
+        let totalReviews = listing.reviews.length + 1;
+        listing.reviews.map((review) => {
+            totalStars += review.SpaceRating;
+        });
 
-    totalStars += rating;
-    const averageRating = totalStars / totalReviews;
+        totalStars += rating;
+        const averageRating = totalStars / totalReviews;
 
-    const newReview = {
-      username: "ScarlettRocks!",
-      ownerRating: 5,
-      SpaceRating: rating,
-      Body: body,
+        const newReview = {
+            username: "ScarlettRocks!",
+            ownerRating: 5,
+            SpaceRating: rating,
+            Body: body,
+        };
+
+        const updatedReviewArr = [...listing.reviews, newReview];
+
+        patchListingById(listing._id, {
+            spaceRating: averageRating,
+            reviews: updatedReviewArr,
+        });
     };
+    return (
+        <View style={styles.container}>
+            <Formik
+                validationSchema={ReviewSchema}
+                initialValues={{
+                    rating: "",
+                    body: "",
+                }}
+                onSubmit={(values, actions) => {
+                    if (defaultRating === 0) alert("please rate");
+                    else {
+                        alert("Thanks for your feedback!");
+                        setOpenReviewModal(false);
+                        values.rating = defaultRating;
+                        reviewHandler(values);
+                        actions.resetForm();
+                    }
+                }}
+            >
+                {(props) => (
+                    <View style={styles.review}>
+                        <View style={styles.modalClose}>
+                            <AntDesign
+                                name="close"
+                                size={24}
+                                color="black"
+                                onPress={() => setOpenReviewModal(false)}
+                            />
+                        </View>
+                        <View style={styles.container}>
+                            <StarRating
+                                defaultRating={defaultRating}
+                                setDefaultRating={setDefaultRating}
+                            />
 
-    const updatedReviewArr = [...listing.reviews, newReview];
-
-    patchListingById(listing._id, {
-      spaceRating: averageRating,
-      reviews: updatedReviewArr,
-    });
-  };
-  return (
-    <View style={styles.container}>
-      <Formik
-        validationSchema={ReviewSchema}
-        initialValues={{
-          rating: "",
-          body: "",
-        }}
-        onSubmit={(values, actions) => {
-          if (defaultRating === 0) alert("please rate");
-          else {
-            alert("Thanks for your feedback!");
-            setOpenReviewModal(false);
-            values.rating = defaultRating;
-            reviewHandler(values);
-            actions.resetForm();
-          }
-        }}
-      >
-        {(props) => (
-          <View style={styles.review}>
-            <View style={styles.modalClose}>
-              <AntDesign
-                name="close"
-                size={24}
-                color="black"
-                onPress={() => setOpenReviewModal(false)}
-              />
-            </View>
-            <View style={styles.container}>
-              <StarRating
-                defaultRating={defaultRating}
-                setDefaultRating={setDefaultRating}
-              />
-
-              <Text style={styles.errorText}>
-                {props.touched.rating && props.errors.rating}
-              </Text>
-              {/* <Text>Rate your stay!</Text>
+                            <Text style={styles.errorText}>
+                                {props.touched.rating && props.errors.rating}
+                            </Text>
+                            {/* <Text>Rate your stay!</Text>
               <TextInput
                 style={styles.rating}
                 placeholder="Rating, between 1 - 5"
@@ -101,75 +101,78 @@ export default function ReviewModal({ setOpenReviewModal, listing }) {
                 value={props.values.rating}
                 onBlur={props.handleBlur("rating")}
               /> */}
-              <Text style={styles.review}>Leave your Review</Text>
-              <TextInput
-                style={styles.body}
-                multiline
-                placeholder="Write your review here"
-                onChangeText={props.handleChange("body")}
-                value={props.values.body}
-                onBlur={props.handleBlur("body")}
-              />
-              <Text style={styles.errorText}>
-                {props.touched.body && props.errors.body}
-              </Text>
+                            <Text style={styles.review}>Leave your Review</Text>
+                            <TextInput
+                                style={styles.body}
+                                multiline
+                                placeholder="Write your review here"
+                                onChangeText={props.handleChange("body")}
+                                value={props.values.body}
+                                onBlur={props.handleBlur("body")}
+                            />
+                            <Text style={styles.errorText}>
+                                {props.touched.body && props.errors.body}
+                            </Text>
 
-              <TouchableOpacity
-                activeOpacity={0.7}
-                style={styles.buttonStyle}
-                onPress={props.handleSubmit}
-              >
-                <Text style={styles.buttonTextStyle}>Submit</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
-      </Formik>
-    </View>
-  );
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                style={styles.buttonStyle}
+                                onPress={props.handleSubmit}
+                            >
+                                <Text style={styles.buttonTextStyle}>
+                                    Submit
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+            </Formik>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  modalClose: { marginTop: 40, alignItems: "center" },
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 40,
-  },
-  review: {
-    marginTop: 30,
-    padding: 8,
-    fontSize: 18,
-    textAlign: "center",
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  rating: {
-    height: 30,
-    borderWidth: 1,
-    marginBottom: 50,
-  },
-  body: {
-    width: "80%",
-    borderWidth: 1,
-    height: 120,
-  },
-  buttonStyle: {
-    justifyContent: "center",
-    flexDirection: "row",
-    marginTop: 30,
-    padding: 15,
-    backgroundColor: "#8ad24e",
-  },
-  buttonTextStyle: {
-    color: "#fff",
-    textAlign: "center",
-  },
-  errorText: {
-    color: "crimson",
-    fontWeight: "bold",
-    marginBottom: 10,
-    marginTop: 10,
-    textAlign: "center",
-  },
+    modalClose: { marginTop: 0, alignItems: "center" },
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10,
+    },
+    review: {
+        marginTop: 30,
+        padding: 8,
+        fontSize: 18,
+        textAlign: "center",
+        fontWeight: "bold",
+        marginBottom: 20,
+    },
+    rating: {
+        height: 30,
+        borderWidth: 1,
+        marginBottom: 50,
+    },
+    body: {
+        width: "80%",
+        borderWidth: 1,
+        height: 120,
+    },
+    buttonStyle: {
+        backgroundColor: "#0782F9",
+        width: "50%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+        marginTop: 40,
+    },
+    buttonTextStyle: {
+        color: "#fff",
+        textAlign: "center",
+    },
+    errorText: {
+        color: "crimson",
+        fontWeight: "bold",
+        marginBottom: 10,
+        marginTop: 10,
+        textAlign: "center",
+    },
 });
