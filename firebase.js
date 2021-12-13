aee6b622c1c468e9462fd0866a654e2c9cbb658b
popatre <76402245+popatre@users.noreply.github.com>
2021-12-13 10:15:08 +0000

    add .env config for firebase

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { REACT_APP_FIREBASEAPI } from "@env";
+
 // https://firebase.google.com/docs/web/setup#available-libraries
+const API_KEY = REACT_APP_FIREBASEAPI;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBwhgu9mFun3uI5-r_kjv2o_QHUnz_53Pk",
+    apiKey: API_KEY,
     authDomain: "authapp2-7fbad.firebaseapp.com",
     projectId: "authapp2-7fbad",
     storageBucket: "authapp2-7fbad.appspot.com",
     messagingSenderId: "883766256456",
     appId: "1:883766256456:web:57cff70000bd5069c5497d",
 };
-
 // Initialize Firebase
 // let app;
 // if (app === undefined) {
