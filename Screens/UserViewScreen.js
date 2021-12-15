71b32ce48e8501bc63466dd0930366e6a337130e
popatre <76402245+popatre@users.noreply.github.com>
2021-12-15 11:43:28 +0000

    add basic styling to user profile

--- Screens/UserViewScreen.js
+++ Screens/UserViewScreen.js
@@ -9,47 +9,98 @@ import {
     StyleSheet,
     Modal,
     TextInput,
+    FlatList,
 } from "react-native";
 
 import {
     getUserById,
     getUserByUsername,
     patchUser,
+    getListingsByUsername,
 } from "../utils/apiRequests";
+import ListingCard from "../Components/ListingCard";
 
 const UserViewScreen = ({ route, navigation }) => {
     const { owner } = route.params;
+    const [userProfile, setUserProfile] = useState({});
+    const [userListings, setUserListings] = useState([]);
 
     useEffect(() => {
-        getUserByUsername(owner).then((user) => {});
+        getUserByUsername(owner).then((user) => {
+            setUserProfile(user);
+        });
     }, [owner]);
 
+    const handleListings = () => {
+        getListingsByUsername(userProfile.username).then((listings) => {
+            setUserListings(listings);
+        });
+    };
+
+    const userTitle = "View " + userProfile.displayName + "'s listings";
     return (
-        <View style={{ flexDirection: "row" }}>
-            <Button
-                onPress={() => navigation.navigate("MyListings")}
-                title="User listings"
-            />
-            {/* <View>
-                <Image
-                    style={{ width: 200, height: 200 }}
-                    source={{
-                        uri: owner.avatar,
-                    }}
-                />
+        <View>
+            <View style={{ flexDirection: "row" }}>
+                <View style={styles.rightContainer}>
+                    <Image
+                        style={{
+                            width: 200,
+                            height: 200,
+                            borderRadius: 10,
+                            marginLeft: 5,
+                        }}
+                        source={{
+                            uri: userProfile.avatar,
+                        }}
+                    />
+                </View>
+                <View style={styles.leftContainer}>
+                    <Text style={styles.title}>Display Name:</Text>
+                    <Text style={styles.labelText}>
+                        {userProfile.displayName}{" "}
+                    </Text>
+
+                    <Text style={styles.title}>Username:</Text>
+                    <Text style={styles.labelText}>{userProfile.username}</Text>
+                </View>
             </View>
             <View>
-                <Text>Display Name:{owner.displayName} </Text>
-
-                <Text>Username: {owner.username}</Text>
-                {user === owner.username ? (
-                    <Text>Email: {owner.emailAddress}</Text>
-                ) : null}
-            </View> */}
+                <Button onPress={handleListings} title={userTitle} />
+            </View>
+            <View style={styles.userListings}>
+                <FlatList
+                    keyExtractor={(item) => item._id["$oid"]}
+                    data={userListings}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate("SingleList", {
+                                    id: item._id,
+                                    setUserListings: setUserListings,
+                                });
+                            }}
+                        >
+                            <ListingCard
+                                title={item.title}
+                                location={item.location.city}
+                                price={item.price}
+                                rating={item.spaceRating}
+                                size={item.size}
+                                images={item.images[0]}
+                            />
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
         </View>
     );
 };
 
 export default UserViewScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    labelText: { fontSize: 16, marginBottom: 7 },
+    title: { fontSize: 15, fontWeight: "bold", marginBottom: 3 },
+    leftContainer: { flex: 0.6, marginTop: 10 },
+    rightContainer: { flex: 0.8, marginTop: 10 },
+});
