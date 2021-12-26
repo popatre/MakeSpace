7fede63666b2b73e2fbe26b186d03f2e7027878e
catcodingcat <88160832+catcodingcat@users.noreply.github.com>
2021-12-26 20:54:07 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { REACT_APP_FIREBASEAPI } from "@env";
 import { getStorage } from "firebase/storage";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
 const API_KEY = REACT_APP_FIREBASEAPI;
 
 // Your web app's Firebase configuration
@@ -15,13 +14,7 @@ const firebaseConfig = {
   messagingSenderId: "883766256456",
   appId: "1:883766256456:web:57cff70000bd5069c5497d",
 };
-// Initialize Firebase
-// let app;
-// if (app === undefined) {
-//     app = initializeApp(firebaseConfig);
-// } else {
-//     app = app();
-// }
+
 export const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
